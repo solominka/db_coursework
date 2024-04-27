dc8b4254ccb753a12ede0ead73bcecdab4516f3f
introvertess <solominka21veka@gmail.com>
2024-04-27 23:35:41 +0300

    add account creation

--- service/id_generator.py
+++ service/id_generator.py
@@ -1,11 +1,26 @@
 import shortuuid
 
+from db.repository.account_number_sequence_repository import AccountNumberSequenceRepository
+
 
 class IdGenerator:
-    PRODUCT_PREFIXES = {
+    AGREEMENT_ID_PREFIXES = {
         'current_account': 'ca_',
         'savings_account': 'sa_'
     }
 
+    ACCOUNT_BALANCE_POSITIONS = {
+        'current_account': ['47423', '40903'],
+        'savings_account': ['42301', '47411', '47422', '47423']
+    }
+
+    def __init__(self, ydb_driver):
+        self.__accountNumberSequenceRepository = AccountNumberSequenceRepository(ydb_driver=ydb_driver)
+
     def generate_agreement_id(self, product):
-        return self.PRODUCT_PREFIXES[product] + shortuuid.uuid()
+        return self.AGREEMENT_ID_PREFIXES[product] + shortuuid.uuid()
+
+    def generate_account_numbers(self, product):
+        balance_positions = self.ACCOUNT_BALANCE_POSITIONS[product]
+        account_numbers = self.__accountNumberSequenceRepository.get_next_numbers(balance_positions)
+        return [it['balance_position'] + str(it['current_value']).zfill(15) for it in account_numbers]
